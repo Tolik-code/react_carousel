c6a0200bbdf7cbea051846a23ad562f0e9b8d9c0
Tolik <tolikdolinskiy@gmail.com>
2021-07-11 19:17:27 +0300

    Updates

---
+++ precache-manifest.38696072cfbdf0a126a6e0a7385055ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6130ffab0c2b2b1a4c456576701026c0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6d0f415fb4ebc6c7c86d",
+    "url": "./static/css/main.aed4821b.chunk.css"
+  },
+  {
+    "revision": "32490ed695cf89e93ccb",
+    "url": "./static/js/2.421f2eac.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.421f2eac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d0f415fb4ebc6c7c86d",
+    "url": "./static/js/main.47883d34.chunk.js"
+  },
+  {
+    "revision": "cfec1821169abfb78760",
+    "url": "./static/js/runtime-main.182821f8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c3eae33d7ac1f65680fe9ea5b458330e.js"
+  "./precache-manifest.38696072cfbdf0a126a6e0a7385055ae.js"
 );
 
 self.addEventListener('message', (event) => {
